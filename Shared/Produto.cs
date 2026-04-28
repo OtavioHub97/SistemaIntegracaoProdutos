28b5b15bed86fb49c65fbeb84fd3a1db2723f234
Otavio Henrique <0000290401@senaimgaluno.com.br>
2026-04-28 20:49:31 -0300

    Definindo propriedades da classe Produto.cs no Shared.

---
+++ Shared/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shared
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public decimal Preco { get; set; }
+        public int QuantidadeEstoque { get; set; }
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+    }
+}
--- SistemaIntegracaoProdutos.API/Program.cs
+++ SistemaIntegracaoProdutos.API/Program.cs
@@ -15,7 +15,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-///sjdguagduagfduadab
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
