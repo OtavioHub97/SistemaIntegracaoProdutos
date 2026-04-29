7368edc4c9520c331e289fa19dccc1506931cdf6
Otavio Henrique <0000290401@senaimgaluno.com.br>
2026-04-29 20:24:16 -0300

    Simulador automatico implementado e api blindada com doc swagger e criaçao do DTO no wpf

--- SistemaIntegracaoProdutos.API/Program.cs
+++ SistemaIntegracaoProdutos.API/Program.cs
@@ -11,7 +11,25 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Minha API de Produtos",
+        Version = "v1",
+        Description = "API para integração e persistência de produtos."
+    });
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    if (File.Exists(xmlPath))
+    {
+        c.IncludeXmlComments(xmlPath);
+    }
+    else
+    {
+        Console.WriteLine($" Aviso: Arquivo de documentação XML não encontrado em: {xmlPath}");
+    }
+});
 
 var app = builder.Build();
 
--- SistemaSimulator/Program.cs
+++ SistemaSimulator/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace SistemaSimulator
+﻿using System.Net.Http.Json;
+
+const string apiUrl = "https://localhost:7009/api/produtos";
+using HttpClient client = new HttpClient();
+
+Console.WriteLine("SIMULADOR DE ESTOQUE ATIVO");
+
+
+string[] nomesExemplo = { "Teclado", "Mouse", "Monitor", "Cabo HDMI", "Headset", "Gabinete" };
+Random random = new Random();
+
+while (true)
 {
-    internal class Program
+    // Gera dados aleatórios
+    var novoProdutoDto = new
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Nome = $"{nomesExemplo[random.Next(nomesExemplo.Length)]} {random.Next(100, 999)}",
+        Preco = (decimal)(random.NextDouble() * 500 + 10),
+        QuantidadeEstoque = random.Next(1, 50)
+    };
+
+    try
+    {
+        var response = await client.PostAsJsonAsync(apiUrl, novoProdutoDto);
+        if (response.IsSuccessStatusCode)
+            Console.WriteLine($"[ {DateTime.Now:HH:mm:ss} ] Enviado: {novoProdutoDto.Nome} | Preço: {novoProdutoDto.Preco:C2}");
+        else
+            Console.WriteLine($"[ {DateTime.Now:HH:mm:ss} ] Erro ao enviar.");
     }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Falha de conexão: {ex.Message}");
+    }
+
+    await Task.Delay(2000);
 }
